6d64219d5bfd68e61f3f294dfa7faedfe6b5ad59
illiababich <illia.babich.2@gmail.com>
2023-11-29 18:50:30 +0200

    P238, 2ms, 51.30mb

--- src/main/java/leetcode/P238.java
+++ src/main/java/leetcode/P238.java
@@ -26,9 +26,4 @@ public class P238 {
 
         return result;
     }
-
-    public static void main(String[] args) {
-        int[] numbers = {1,2,3,4};
-        System.out.println(Arrays.toString(productExceptSelf(numbers)));
-    }
 }
